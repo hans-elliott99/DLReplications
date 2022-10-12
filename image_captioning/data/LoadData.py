cf0ece0396de2ce7e3fe4a39c97da521f4eec711
Hans Elliott <hanselliott61@gmail.com>
2022-10-12 02:52:47 -0700

    get ShowAttendTell working

--- image_captioning/data/LoadData.py
+++ image_captioning/data/LoadData.py
@@ -14,10 +14,12 @@ class ImageCaptionDataset(torch.utils.data.Dataset):
         self.X_paths = X_paths
         self.y_labels = y_labels
         self.transforms = transforms
-        self.most_words = len(max([lab.split() for lab in y_labels], key=len)) + 1 ##for the stop token
+        self.most_words = len(max([lab.split() for lab in y_labels], key=len)) + 2 ##for the start/stop tokens
         
         self.stoi_config = stoi_config
+        self.start_tok_idx = stoi_config.stoi[stoi_config.start_token]
         self.stop_tok_idx = stoi_config.stoi[stoi_config.stop_token]
+        self.pad_tok_idx = stoi_config.stoi[stoi_config.pad_token]
     
     def __len__(self):
         assert len(self.X_paths)==len(self.y_labels)
@@ -32,7 +34,7 @@ class ImageCaptionDataset(torch.utils.data.Dataset):
         # LABEL
         str_label = self.y_labels[idx]
         str_label = ''.join([c for c in str_label if c not in self.stoi_config.remove_punct])
-        int_label = [self.stoi_config.stoi[s] for s in str_label.split()] + [self.stop_tok_idx]
+        int_label = [self.start_tok_idx] + [self.stoi_config.stoi[s] for s in str_label.split()] + [self.stop_tok_idx]
 
         return img, torch.tensor(self._pad_label(int_label))
 
@@ -43,19 +45,22 @@ class ImageCaptionDataset(torch.utils.data.Dataset):
         return img_tensor
 
     def _pad_label(self, int_label):
-        int_label += [self.stop_tok_idx] * (self.most_words - len(int_label))
+        int_label += [self.pad_tok_idx] * (self.most_words - len(int_label))
         return int_label
 
 
 
+
+
 class StringInt:
-    def __init__(self, labels, start_token, stop_token, remove_punct=""):
+    def __init__(self, labels, start_token, stop_token, pad_token, remove_punct=""):
         """
         Encodes strings as integers and saves useful information about the string-integer mappings.
 
         labels = list of all labels (in train, valid, test) to be used to generate the vocabulary.\n
         start_token = the special token used to start a phrase, such as <sos>\n
         stop_token = the special token used to stop a phrase, such as <eos> (should not be same as start)\n
+        pad_token = the special token used to pad phrases to the same length\n
         remove_punct = a single string listing punctuation to remove from the labels before encoding them\n
 
         Call with a str to return an int, and call with an int to return a str.
@@ -63,14 +68,16 @@ class StringInt:
         all_words = ' '.join(labels)
         all_words = ''.join([c for c in all_words if c not in remove_punct])
         unique_words = sorted(set(all_words.lower().split()))
-        stoi = {word:i+2 for i,word in enumerate(unique_words)}
-        stoi[start_token] = 1
-        stoi[stop_token] = 0
+        stoi = {word:i+3 for i,word in enumerate(unique_words)}
+        stoi[start_token] = 0
+        stoi[stop_token] = 1
+        stoi[pad_token] = 2
 
         self.stoi = stoi
         self.itos = {i:s for s,i in stoi.items()}
         self.start_token=start_token
         self.stop_token=stop_token
+        self.pad_token=pad_token
         self.remove_punct=remove_punct
     
     def __len__(self):
---
+++ image_captioning/models/ShowAttendTell.py
@@ -0,0 +1,176 @@
+import torch
+import torchvision
+from data.LoadData import StringInt
+
+class Encoder(torch.nn.Module):
+    def __init__(self, encoded_image_size=14):
+        super(Encoder, self).__init__()
+        
+        self._init_resnet()
+
+        # Resize image to fixed size to allow input images of variable size
+        ## (encoded 'images' will be of size 14x14)
+        self.adaptive_pool = torch.nn.AdaptiveAvgPool2d((encoded_image_size, encoded_image_size))
+
+
+    def _init_resnet(self): ##add similair method to load any different torchvision model if desired
+        # Load weights
+        res_weights = torchvision.models.ResNet50_Weights.IMAGENET1K_V2
+        # Save preprocessing info
+        self.encoder_transforms = res_weights.transforms()
+        # Load model
+        resnet = torchvision.models.resnet50(weights=res_weights)
+        # Remove model head
+        layers = list(resnet.children())[:-2]
+        self.resnet = torch.nn.Sequential(*layers)
+    
+    def forward(self, images):
+        out = self.resnet(images)   ##(batch, 2048, image_size/32, image_size/32)
+        out = self.adaptive_pool(out)
+        return out.permute(0, 2, 3, 1) ##(batch, image_size/32, image_size/32, 2048)
+
+    # def finetune()
+ 
+
+
+class SoftAttention(torch.nn.Module):
+    """
+    Inspired by Bahdanau et al (2015) - see pg. 12 for key details
+    """
+    def __init__(self, enc_output_dim, dec_hidden_dim, attention_dim, activ_fn=torch.nn.ReLU):
+        super(SoftAttention, self).__init__()
+
+        self.enc2att = torch.nn.Linear(enc_output_dim, attention_dim, bias=False)    #map from encoded images to attention    
+        self.hidden2att = torch.nn.Linear(dec_hidden_dim, attention_dim, bias=False) #map from decoder hidden state to attention
+        self.att = torch.nn.Linear(attention_dim, 1)
+
+        self.activ_fn = activ_fn() ##paper uses tanh
+        self.softmax = torch.nn.Softmax(dim=1)
+
+    def forward(self, encoder_output, dec_hidden):
+        att1 = self.enc2att(encoder_output)                ##(batch_size, num_pixels, attention_dim)
+        att2 = self.hidden2att(dec_hidden).unsqueeze(1)    ##(batch_size, 1, attention_dim)
+
+        att = self.att(self.activ_fn(att1 + att2)).squeeze(2) ##(batch_size, num_pixels)
+        alpha = self.softmax(att) ##(batch_size, num_pixels)  - add 3rd dimension to broadcast alpha across all encoded feature maps
+        context = (encoder_output * alpha.unsqueeze(2)).sum(dim=1) ##(batch_size, encoder_dim)
+        return context, alpha
+
+
+        
+class Decoder(torch.nn.Module):
+    def __init__(self, embed_dim, dec_hidden_dim, attention_dim, stoi_config:StringInt,
+                enc_output_dim=2048, activ_fn=torch.nn.ReLU, dropout=0.0
+    ) -> None:
+        """
+        
+        enc_output_dim: the number of feature maps produced by the encoder (2048 if ResNet)\n
+        """
+        super(Decoder, self).__init__()
+
+        self.device = torch.device("cpu")
+        #params
+        self.embed_dim = embed_dim
+        self.dec_hidden_dim = dec_hidden_dim
+        self.attention_dim = attention_dim
+        self.enc_output_dim = enc_output_dim
+
+        #data specs
+        self.vocab_size = len(stoi_config)
+        self.pad_tok_idx = stoi_config.stoi[stoi_config.pad_token]
+        
+        #core model
+        self.attention = SoftAttention(enc_output_dim=enc_output_dim, dec_hidden_dim=dec_hidden_dim,
+                                        attention_dim=attention_dim, activ_fn=activ_fn)
+        self.output_embed = torch.nn.Embedding(self.vocab_size, embed_dim) #word embeddings
+        self.dropout = torch.nn.Dropout(p=dropout)
+        self.rnn_decode = torch.nn.LSTMCell(embed_dim+enc_output_dim, dec_hidden_dim, bias=True)
+        self.f_beta = torch.nn.Linear(dec_hidden_dim, enc_output_dim) #layer for sigmoid gate post-rnn
+        self.sigmoid = torch.nn.Sigmoid()
+        self.fc_head = torch.nn.Linear(dec_hidden_dim, self.vocab_size) #layer to compute probability dist over vocab
+
+        #hidden state + weight init
+        self.init_h = torch.nn.Linear(enc_output_dim, dec_hidden_dim) ##learnable layers for initializing hidden states
+        self.init_c = torch.nn.Linear(enc_output_dim, dec_hidden_dim)
+        # self._init_weights()
+
+    def _init_weights(self):
+        """
+        Prep specific params with better weight initialization for better convergence.
+        """
+        self.output_embed.weight.data.uniform_(-0.1, 0.1)
+        self.fc_head.bias.data.fill_(0)
+        self.fc_head.weight.data.uniform_(-0.1, 0.1)
+
+    def _init_hidden(self, encoder_output):
+        """
+        Initalize first hidden states with the means of the encoded images
+        """
+        mean_encoded = encoder_output.mean(dim=1)
+        h = self.init_h(mean_encoded) ##(batch_size, num_pixels, dec_hidden_dim)
+        c = self.init_c(mean_encoded) ##(same)
+        return h, c
+
+
+    def forward(self, encoder_output, y_encodings):
+        self.device = torch.device("cpu")
+        batch_size = encoder_output.size(0)   
+        encoder_dim = encoder_output.size(-1) ##number of feature maps
+        caption_dim = y_encodings.size(1)
+
+        # Flatten encoded images
+        encoder_output = encoder_output.view(batch_size, -1, encoder_dim) ##(batch_size, num_pixels, encoder_dim)
+        num_pixels = encoder_output.size(1)
+
+        # Sort input data by decreasing length to avoid using pad tokens
+        # Also calculate decode lengths - the proper length of decoded output for each caption 
+        caption_lengths = torch.tensor(
+            [len([w.item() for w in caption if w != self.pad_tok_idx]) for caption in y_encodings],
+            device=self.device
+            )
+        caption_lengths, sort_idx = caption_lengths.sort(dim=0, descending=True)
+
+        encoder_output = encoder_output[sort_idx]
+        y_encodings = y_encodings[sort_idx]
+
+        decode_lengths = (caption_lengths - 1).tolist() ##- 1 since we don't run the 'end' token through the decoder 
+        
+        # Output Word Embeddings
+        embed = self.output_embed(y_encodings) ##(batch_size, vocab_size, embed_dim)
+
+        # Initialize hidden states
+        hidden, cell = self._init_hidden(encoder_output=encoder_output) ##(batch_size, num_pixels, dec_hidden_dim)
+        
+        # Tensors to hold word prediction scores, alphas
+        predictions = torch.zeros((batch_size, max(decode_lengths), self.vocab_size), device=self.device)
+        alphas = torch.zeros((batch_size, max(decode_lengths), num_pixels), device=self.device)
+
+
+        # Decoder Recurrent Net
+        for t in range(max(decode_lengths)):
+            # Determine effective batch size for time step t (to start it will be batch_size)
+            batch_size_t = sum([dec_len > t for dec_len in decode_lengths]) ##calculate 'effective batch size' for time step t
+            # Forward pass the attention network to calculate the context vector (ie, the attention weighted image encodings)
+            context_vec, alpha = self.attention.forward(encoder_output[:batch_size_t],       ##context_vec (batch_size, num_pixels, encoder_dim)
+                                                        dec_hidden=hidden[:batch_size_t])    ##
+            
+            # Calculate and apply a sigmoid gate (paper sec. 4.2.1) which may help the model attend
+            gate = self.sigmoid(self.f_beta(hidden[:batch_size_t]))
+            context_vec = gate * context_vec
+            
+            # Perform a decoding step with the recurrent net to update hidden states
+            ## Pass in the Input: a concat of the embeddings (for the effective batch) with the contect vector
+            ## and a tuple, (hidden state, cell state)
+            hidden, cell = self.rnn_decode(
+                torch.cat((embed[:batch_size_t, t, :], context_vec), dim=1),
+                (hidden[:batch_size_t], cell[:batch_size_t])
+            ) ## (batch_size_t, vocab_size)
+
+            # Pass hidden state through a FC layer to calculate output logits
+            logits = self.fc_head(self.dropout(hidden)) ##(batch_size_t, vocab_size)
+
+            # Add predictions and alphas to save-tensors
+            predictions[:batch_size_t, t, :] = logits
+            alphas[:batch_size_t, t, :] = alpha
+        
+        return predictions, alphas, decode_lengths
