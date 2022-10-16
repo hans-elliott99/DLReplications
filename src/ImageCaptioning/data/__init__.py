933e5d264c1a239ea0c363b0fad67ddea2e2fb24
Hans Elliott <91705409+hans-elliott99@users.noreply.github.com>
2022-10-16 00:30:02 -0700

    Delete src directory

--- src/ImageCaptioning/data/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import LoadData 
-from . import DownloadData
--- src/ImageCaptioning/temp.py
+++
@@ -1,4 +0,0 @@
-from ..ImageCaptioning.train import get_config_layout
-from ..ImageCaptioning.data import LoadData
-
-print(get_config_layout())
